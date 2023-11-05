b38f20f4b94fd383af6c007fca2bfa239bd1a9cd
19B9052 <19b9052@ubd.edu.bn>
2023-11-05 22:15:35 +0800

    Update 19B9052

--- Azri.m
+++ Azri.m
@@ -1,7 +1,7 @@
 % The x and y coordinate for creating the word 'Azri'
-x = [3.00 2.50 2.00 1.00 1.00 3.50 5.00 3.50 5.00 6.00 6.00 5.90 ... 
+x4 = [3.00 2.50 2.00 1.00 1.00 3.50 5.00 3.50 5.00 6.00 6.00 5.90 ... 
     6.50 7.00 6.20 6.20 7.00 7.50 7.50 8.00 7.80 7.50];
-y = [1.00 2.50 4.00 1.00 2.50 2.50 2.50 1.00 1.00 1.00 3.00 2.50 ...
+y4 = [1.00 2.50 4.00 1.00 2.50 2.50 2.50 1.00 1.00 1.00 3.00 2.50 ...
     3.00 2.80 2.50 1.00 1.00 1.00 2.50 3.00 3.50 3.00];
 
 n = length(x);
@@ -9,14 +9,14 @@ t = 0:n-1; % Parametric coordinate t
 tt = 0:0.05:n-1; % More precise coordination for t spline interpolation
 
 % Using the spline interpolation
-xx = spline(t, x, tt);
-yy = spline(t, y, tt);
+xx = spline(t, x4, tt);
+yy = spline(t, y4, tt);
 
 % Plot the graph 
 figure(1)
 plot(xx, yy, 'r', 'LineWidth', 1.5) % for spline
 hold on
-plot(x, y, 'ko', 'MarkerFaceColor', 'k')
+plot(x4, y4, 'ko', 'MarkerFaceColor', 'k')
 axis([-0.2 6 -0.2 4.5])
 grid on
 title('Spline Interpolation of Cursive Word Azri')
